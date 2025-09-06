b05bb5e0a0deb7df70027ad2b06c705707a5f36b
elijahmendezcs <elijahmendez2022@gmail.com>
2025-09-06 12:15:53 -0400

    updates

--- solutions/two_pointers/problem_125_valid_palindrome.py
+++ solutions/two_pointers/problem_125_valid_palindrome.py
@@ -1,10 +1,21 @@
-class Solution:
-    def isPalindrome(self, s: str) -> bool:
-        newStr = ""
+class Solution(object):
+    def isPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        # w 2 pointer approach
 
-        for c in s:
-            if c.isalnum():
-                newStr += c.lower()
-        return newStr == newStr[::-1]
-            
-  # two pointer approach! 
+        i = 0
+        j = len(s) - 1
+
+        while i < j:
+            while i < j and not s[i].isalnum():
+                i += 1
+            while i < j and not s[j].isalnum():
+                j -= 1
+            if s[i].lower() != s[j].lower():
+                return False
+            i += 1
+            j -= 1
+        return True
