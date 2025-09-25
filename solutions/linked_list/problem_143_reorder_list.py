4fe121eabdfccba36860f09d8b58df6c357f0b76
Elijah Mendez <elijahmendez2022@gmail.com>
2025-09-24 22:32:26 -0400

    updates

--- solutions/linked_list/problem_143_reorder_list.py
+++ solutions/linked_list/problem_143_reorder_list.py
@@ -49,3 +49,55 @@ class Solution:
             # move both pointers forward
             first = tmp1
             second = tmp2
+
+
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reorderList(self, head: Optional[ListNode]) -> None:
+        """
+        Do not return anything, modify head in-place instead.
+        """
+
+        # Fast and Slow Pointers
+        slow = head
+        fast = head.next
+        while fast and fast.next:
+            slow = slow.next # slow goes + 1
+            fast = fast.next.next # fast goes + 2
+
+
+        # splitting the list into halves
+        # second starts at the node after the middle
+        # cut the list in half by setting slow.next = None
+        second = slow.next
+        slow.next = None
+
+        # Reverse the second half of the list
+        prev = None
+        while second: # reversing a linked list
+            tmp = second.next
+            second.next = prev
+            prev = second
+            second = tmp
+
+        # now we need to merge the two halves
+        first = head
+        second = prev
+        while second:
+            tmp1 = first.next # store next node from the first half
+            tmp2 = second.next # store next node from second half 
+
+            first.next = second # link current first node to current second node
+            second.next = tmp1 # link current second node to first node
+
+            # move both pointers forward
+            first = tmp1 
+            second = tmp2
+
+
+
