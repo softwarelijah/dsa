df5834aabe96bdc5360c8cb2029bf8cd26e2d8e8
elijahmendezcs <elijahmendez2022@gmail.com>
2025-10-04 19:30:00 -0400

    updates

--- solutions/datadog/bonus/design_add_and_search_words_data_structure.py
+++ solutions/datadog/bonus/design_add_and_search_words_data_structure.py
@@ -1,1 +1,48 @@
-# placeholder
+"""
+Design a data structure that supports adding new words and finding if a string matches any previously added string.
+
+Implement the WordDictionary class:
+
+WordDictionary() Initializes the object.
+void addWord(word) Adds word to the data structure, it can be matched later.
+bool search(word) Returns true if there is any string in the data structure that matches word or false otherwise. word may contain dots '.' where dots can be matched with any letter.
+ 
+
+Example:
+
+Input
+["WordDictionary","addWord","addWord","addWord","search","search","search","search"]
+[[],["bad"],["dad"],["mad"],["pad"],["bad"],[".ad"],["b.."]]
+Output
+[null,null,null,null,false,true,true,true]
+
+Explanation
+WordDictionary wordDictionary = new WordDictionary();
+wordDictionary.addWord("bad");
+wordDictionary.addWord("dad");
+wordDictionary.addWord("mad");
+wordDictionary.search("pad"); // return False
+wordDictionary.search("bad"); // return True
+wordDictionary.search(".ad"); // return True
+wordDictionary.search("b.."); // return True
+"""""
+
+class WordDictionary:
+
+    def __init__(self):
+        return None
+
+
+    def addWord(self, word: str) -> None:
+        return None
+
+
+    def search(self, word: str) -> bool:
+        return False
+
+
+
+# Your WordDictionary object will be instantiated and called as such:
+# obj = WordDictionary()
+# obj.addWord(word)
+# param_2 = obj.search(word)
