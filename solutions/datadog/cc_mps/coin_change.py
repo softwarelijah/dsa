43d37b03f4ade86b9987f6cffe9977462c033c45
elijahmendezcs <elijahmendez2022@gmail.com>
2025-10-04 19:31:10 -0400

    updates

--- solutions/datadog/cc_mps/coin_change.py
+++ solutions/datadog/cc_mps/coin_change.py
@@ -26,20 +26,3 @@ Example 3:
 Input: coins = [1, 5, 10, 25], amount = 0
 Output: 0
 """
-
-class Solution:
-    def coinChange(self, coins: List[int], amount: int ) -> int:
-
-        coins.sort(reverse=True)
-        count = 0
-        remaining = amount
-        for coin in coins:
-            if remaining == 0:
-                break
-            num_coins = remaining // coin
-            count += num_coins
-            remaining -= num_coins * coin
-        return count
-
-sol = Solution()
-print(sol.coinChange([1, 5, 10, 25], 100))
